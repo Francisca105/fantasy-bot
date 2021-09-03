e90b809cb9f55d5834a77711ec0ad581dc457db0
Francisca105 <65908870+Francisca105@users.noreply.github.com>
2021-09-03 11:12:13 +0100

    Delete Eventos directory

--- Eventos/ready.js
+++
@@ -1,4 +0,0 @@
-exports.run = (client) => {
-  client.user.setActivity(`Acesse meu site: fantasyhosting.com.br`);
-  console.log(`Estou online, em ${client.guilds.size} servidores, com ${client.users.size} usuários`);
-}
