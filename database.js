f9301e3e0e38eef9a4949954780e5edcf026328d
Francisca105 <65908870+Francisca105@users.noreply.github.com>
2021-09-03 11:10:24 +0100

    Delete database.js

--- database.js
+++
@@ -1,7 +0,0 @@
-const config = require("./config.json")
-const { Sequelize } = require("sequelize")
-
-module.exports = new Sequelize(config.mysql.database, config.mysql.user, config.mysql.password, {
-    host: config.mysql.host,
-    dialect: 'mysql'
-})
