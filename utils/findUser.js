444219c8e75bacb6f084f97da12cc3603ef4eb66
Francisca105 <65908870+Francisca105@users.noreply.github.com>
2021-09-03 11:09:57 +0100

    Delete utils directory

--- utils/findUser.js
+++
@@ -1,8 +0,0 @@
-const fh = require('../index.js')
-
-module.exports.findUser = (USER, message) => {
-    let user = message.mentions.members.first() || message.guild.members.cache.get(USER) || message.guild.members.cache.find(us => us.user.username === USER) 
-
-    if(!user) return null;
-    else return user;
-}
